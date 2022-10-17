42653e4a499a8e2447825f97043712287b1e00eb
Lucas <ohpa_@hotmail.co.th>
2022-10-18 03:27:00 +0700

    add funtion

--- webapp.py
+++ webapp.py
@@ -1,11 +1,23 @@
+from unicodedata import name
 from flask import Flask,render_template,request
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
 app = Flask(__name__)
+app.config ['SECRET_KEY'] = 'mykey'
 
-@app.route('/')
+class Myform(FlaskForm):
+    name = StringField('ใส่ชื่อ')
+    submit = SubmitField("บันทึก")
 
+
+@app.route('/',methods =['get','post'])
 def index():
-    data = {"name":"lucas","age":"19"}
-    return render_template("index.html",mydata = data)
+    name = False 
+    form = Myform()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template('index.html', form = form,name = name)
 
 @app.route('/shop')
 def shop():
@@ -20,14 +32,25 @@ def admin():
 
 @app.route('/test')
 def testweb():
-    name = "Lucas"
-    return render_template("test.html",name = name)
+    name = Myform()
+    return render_template("test.html",form = name)
 
 @app.route('/sigupForm')
 def sigupForm():
     fname = request.args.get('fname')
     lname = request.args.get('lname')
     return render_template("get.html",data = {"name":fname,"lname":lname})
+
+@app.route('/sandbox')
+def sandbox():
+    name = "Lucas"
+    return render_template("sandbox.html",name = name)
+
+@app.route('/urlsand')
+def testlink():
+    namel = request.args.get('name1')
+    namell = request.args.get('sand')
+    return render_template("show.html",d = {"name":namel,"namell":namell})
 
 if __name__ == "__main__":
  app.run(debug=True)
